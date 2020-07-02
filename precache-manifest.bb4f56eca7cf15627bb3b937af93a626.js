efcb14c8a7cafc0bfbb4de3d8d88422f863cce08
rakuthesenpai <raul.r.h.rosas@gmail.com>
2020-07-01 17:17:38 -0700

    Updates

---
+++ precache-manifest.bb4f56eca7cf15627bb3b937af93a626.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea29d5072502f7dbe1714d5e9964c7a",
+    "url": "/CsGoViewer/index.html"
+  },
+  {
+    "revision": "c395e9ce3ca9d1154e55",
+    "url": "/CsGoViewer/static/css/main.1dcf13bd.chunk.css"
+  },
+  {
+    "revision": "446c163dfae4548daf7a",
+    "url": "/CsGoViewer/static/js/2.1d3ab15b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/CsGoViewer/static/js/2.1d3ab15b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c395e9ce3ca9d1154e55",
+    "url": "/CsGoViewer/static/js/main.b98b821e.chunk.js"
+  },
+  {
+    "revision": "9d4675746a197b1f6105",
+    "url": "/CsGoViewer/static/js/runtime-main.3a0a039c.js"
+  },
+  {
+    "revision": "d44abe859022a34be617ce7650e5093b",
+    "url": "/CsGoViewer/static/media/background.d44abe85.jpg"
+  }
+]);
