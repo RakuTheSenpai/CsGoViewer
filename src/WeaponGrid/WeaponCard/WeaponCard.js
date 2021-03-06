3bc12d3ba704ee34ffaeeac6c3a79fe4b33d25cd
Raku <raul.r.h.rosas@gmail.com>
2021-03-05 16:57:32 -0800

    Hotfix for steam internal changes

--- src/WeaponGrid/WeaponCard/WeaponCard.js
+++ src/WeaponGrid/WeaponCard/WeaponCard.js
@@ -10,6 +10,7 @@ class WeaponCard extends React.Component {
         const weapon = this.props.weapon
         const { name, tags, icon_url } = weapon
         const rarity = tags.filter(tag => tag.category == 'Rarity')[0].color
+        const steam_images_link = 'https://community.akamai.steamstatic.com/economy/image/'
         return (
             <Grid
                 container
@@ -28,7 +29,7 @@ class WeaponCard extends React.Component {
                     <div style={{ backgroundColor: '#' + rarity, width: '100%', height: '100%', overflow: 'hidden' }}>
                         <img
                             className='image-background'
-                            src={'http://cdn.steamcommunity.com/economy/image/' + icon_url}
+                            src={steam_images_link + icon_url}
                             style={{ width: '95%', height: '100%', float: 'right' }}
                         />
                     </div>
